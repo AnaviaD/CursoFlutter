af38cdc029461378b494380669255baf43d20355
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-05 08:33:57 -0600

    Update Getters n setters lesson

--- 9 GettersSetters.dart
+++ 9 GettersSetters.dart
@@ -23,3 +23,40 @@ class Cuadrado{
 // ====================================================================================
 // =================================== Primer Codigo ==================================
 // ====================================================================================
+
+// ====================================================================================
+// =================================== Segundo Codigo =================================
+// ====================================================================================
+void main(){
+  final cuadrado = new Cuadrado(5);
+  
+  print('area: ${cuadrado.calculaArea()}');
+  print('lado: ${cuadrado.lado}');
+  print('area get: ${cuadrado.area}');
+  
+  cuadrado.area = 20;
+}
+
+class Cuadrado{
+  double lado = 0;  //Lado * Lado
+  
+  //   Para los getters se definen como un metodo normal
+  //   La diferencia esta en que los getters no tienen ()
+  double get area{
+    return this.lado * this.lado;
+  }
+  
+  set area(double valor){
+    print('set: ${valor}');
+  }
+  
+  Cuadrado( double lado):
+    this.lado = lado;
+  
+  double calculaArea(){
+    return this.lado * lado;
+  }
+}
+// ====================================================================================
+// =================================== Segundo Codigo =================================
+// ====================================================================================
